337d6856866610d7a046021d8ae285136fd868bd
Boston343 <branden.austin1@gmail.com>
2022-11-26 15:50:56 -0500

    keeper app challenge 1, basic header, footer, and note

--- src/index.js
+++ src/index.js
@@ -1,51 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 import "./css/styles.css";
 import reportWebVitals from "./reportWebVitals";
 
-const fName = "Reap3r";
-const lName = "Chaos";
-const luckyNum = 7;
-const date = new Date();
-// const hour = date.getHours();  // actual
-const hour = 18; // testing
-
-let headerText;
-const headerStyle = {
-  color: "",
-};
-
-if (hour < 12) {
-  // Midnight to Noon
-  headerText = "Good Morning";
-  headerStyle.color = "red";
-} else if (hour < 18) {
-  // Noon to 6PM
-  headerText = "Good Afternoon";
-  headerStyle.color = "green";
-} else {
-  // 6PM to Midnight
-  headerText = "Good Evening";
-  headerStyle.color = "blue";
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <div>
-    <p
-      className="heading"
-      style={headerStyle}
-      spellCheck="false"
-      contentEditable="true"
-    >
-      {headerText}
-    </p>
-    <p>Copyright {date.getFullYear()}.</p>
-    <p>Current Time: {hour}</p>
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
