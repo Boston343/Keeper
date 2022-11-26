60a0cd638f0d44a3cfed3b74433949d535214f03
Boston343 <branden.austin1@gmail.com>
2022-11-26 10:57:57 -0500

    example of inline style editing in React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "./css/styles.css";
 import reportWebVitals from "./reportWebVitals";
 
 const fName = "Reap3r";
 const lName = "Chaos";
 const luckyNum = 7;
 const date = new Date();
+// const hour = date.getHours();  // actual
+const hour = 18; // testing
+
+let headerText;
+const headerStyle = {
+  color: "",
+};
+
+if (hour < 12) {
+  // Midnight to Noon
+  headerText = "Good Morning";
+  headerStyle.color = "red";
+} else if (hour < 18) {
+  // Noon to 6PM
+  headerText = "Good Afternoon";
+  headerStyle.color = "green";
+} else {
+  // 6PM to Midnight
+  headerText = "Good Evening";
+  headerStyle.color = "blue";
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,8 +35,16 @@ root.render(
   //   <App />
   // </React.StrictMode>
   <div>
-    <p>Created by {fName}.</p>
+    <p
+      className="heading"
+      style={headerStyle}
+      spellCheck="false"
+      contentEditable="true"
+    >
+      {headerText}
+    </p>
     <p>Copyright {date.getFullYear()}.</p>
+    <p>Current Time: {hour}</p>
   </div>
 );
 
